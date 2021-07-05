954c68b418bd6c1be9f48939e3d8a9f3b0e7e77b
= <=>
2021-07-05 12:21:14 +0200

    Set up, Initial behaviour, Retailer Clients

--- app/Clients/BestBuy.php
+++ app/Clients/BestBuy.php
@@ -7,20 +7,23 @@ use Illuminate\Support\Facades\Http;
 
 class BestBuy implements Client
 {
-    public function checkAvailability(Stock $stock): StockStatus
+    public function checkAvailability(Stock $stock): StockStatus //This function checks the availability of a Stock
     {
-        $results = Http::get($this->endpoint($stock->sku))->json();
+        $results = Http::get($this->endpoint($stock->sku))->json(); //Uses Http package and get function. As an input the
+        //sku is passed and it is gotten as json format which contains all the information provided by the webpage.
 
         return new StockStatus(
             $results['onlineAvailability'],
-            $this->dollarsToCents($results['salePrice'])
+            $this->dollarsToCents($results['salePrice']) //In the case of Best Buy the field referred to Availability is
+            //called 'onlineAvailability' and it is extracted, then the same goes for 'salePrice'.
+
         );
+
     }
 
     protected function endpoint($sku): string
     {
         $key = config('services.clients.bestBuy.key');
-
         return "https://api.bestbuy.com/v1/products/{$sku}.json?apiKey={$key}";
     }
 
